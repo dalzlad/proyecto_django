47a00f510a0783f84ccb4c6b1ea3904cc7b3b653
dalzlad <dalz.sena@gmail.com>
2023-09-06 20:27:47 -0500

    Config postgress

--- proyectoa_agenda/settings copy.py
+++ proyectoa_agenda/settings copy.py
@@ -113,10 +113,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATIC_URL = 'static/'
 
 # Ruta para las imágenes de cada registro
 MEDIA_URL = 'media/'
